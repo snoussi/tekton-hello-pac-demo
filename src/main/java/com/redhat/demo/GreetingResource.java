67024a6d21def0561468aa0e379e154cf6d11cd5
snoussi <rachid+github@redhat.com>
2025-09-08 20:05:04 +0200

    first commit

---
+++ src/main/java/com/redhat/demo/GreetingResource.java
@@ -0,0 +1,17 @@
+package com.redhat.demo;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Jenkins 🤖";
+        // return "Hello from Tekton 🚀";
+    }
+}
